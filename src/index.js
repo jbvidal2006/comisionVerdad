df09b28eacfc3afac9d22d9fa5a97a60e42b162e
jbvidal2006 <jbvidal2006@gmail.com>
2020-03-25 16:47:32 -0500

    comit 2

--- src/index.js
+++ src/index.js
@@ -16,7 +16,11 @@ app.use(express.json());
 //section routes
 app.use('/api/resource', require('./routes/resource.routes'));
 
-
+//server up static assets (Heroku)
+if(process.env.NODE_ENV == 'production'){
+    console.log("use cient/build form Heroku");
+    app.use(express.static("client/build"));
+}
 
 //Section static files
 app.use(express.static(path.join(__dirname,'public')))
